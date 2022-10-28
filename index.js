b0969fb23f16e10eac1df2b583f7bba34a9e2948
drene <jinadopeyemi@gmail.com>
2022-10-28 18:53:39 +0100

    cors

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 8080
+const cors = require('cors')
+
+app.use(cors());
 
 
 app.get('/', (req, res) => res.json({
